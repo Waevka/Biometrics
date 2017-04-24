05e436085dddf2917e76b8277a3865f60bedf4da
Waevka <waveswallow@gmail.com>
2017-04-24 11:50:47 +0200

    Added basic speech recognition.
    + Updated gitignore and cleaned up the repo.

--- VoiceReco/Assets/Scripts/SpeechRecognizer.cs
+++ VoiceReco/Assets/Scripts/SpeechRecognizer.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
-public class SpeechRecognizer : MonoBehaviour {
-    public GameObject target;
+public enum ActionType { None, Turn };
+public enum ObjectType { None, Cube };
+public delegate void performActionOnObject(GameObject o);
 
+public class SpeechRecognizer : MonoBehaviour {
+
+    public GameObject target;
     public string[] keywords = new string[]{"turn"};
     public ConfidenceLevel confidenceLevel = ConfidenceLevel.Medium;
+
     protected PhraseRecognizer phraseRecognizer;
-    protected string word = "none";
+    protected string actionWord = "none";
+    protected string objectWord = "none";
+    protected ActionType action = ActionType.None;
+    protected ObjectType obj    = ObjectType.None; 
+    performActionOnObject performAction;
 
 	// Use this for initialization
 	void Start () {
-        if (keywords != null)
+        if (keywords != null && PhraseRecognitionSystem.isSupported)
         {
             Debug.Log(PhraseRecognitionSystem.isSupported);
             phraseRecognizer = new KeywordRecognizer(keywords, confidenceLevel);
             phraseRecognizer.OnPhraseRecognized += onPhraseRecognized;
             phraseRecognizer.Start();
             Debug.Log(PhraseRecognitionSystem.Status);
+        } else
+        {
+            if (!PhraseRecognitionSystem.isSupported)
+            {
+                Debug.Log("Phrase Recognition seems to be not supported by your system. Please use Windows 10 and change your language settings to English.");
+            }
         }
 	}
 	
@@ -30,17 +45,60 @@ public class SpeechRecognizer : MonoBehaviour {
 
     private void onPhraseRecognized(PhraseRecognizedEventArgs args)
     {
-        word = args.text;
-        if(word == "turn")
+        actionWord = args.text;
+        //object = args.text....
+
+        processActionName();
+        processObjectName();
+
+        if (performAction != null && target != null)
         {
-            turnCube();
+            performAction(target);
         }
     }
 
-    private void turnCube()
+    #region PhraseProcessor
+
+    private void processActionName()
     {
-        target.transform.Rotate(new Vector3(0,0,45.0f));
+        switch (actionWord)
+        {
+            case "turn":
+                action = ActionType.Turn;
+                performAction = rotateObject;
+                break;
+            default:
+                action = ActionType.None;
+                performAction = null;
+                break;
+        }
     }
+
+    private void processObjectName()
+    {
+        switch (objectWord)
+        {
+            case "cube":
+                obj = ObjectType.Cube;
+                //target = cube
+                break;
+            default:
+                obj = ObjectType.None;
+                //target = null;
+                break;
+        }
+    }
+
+    #endregion
+
+    #region ObjectActions
+
+    private void rotateObject(GameObject o)
+    {
+        o.transform.Rotate(new Vector3(0,0,45.0f));
+    }
+
+    #endregion
 
     private void OnApplicationQuit()
     {
