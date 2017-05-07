89eef78cf2eeb7657f546c38882789d42f2d0a04
Waevka <waveswallow@gmail.com>
2017-05-08 00:57:45 +0200

    Fixes to PhraseRecognition.

--- VoiceReco/Assets/Scripts/SpeechRecognizer.cs
+++ VoiceReco/Assets/Scripts/SpeechRecognizer.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
-public enum ActionType { None, Turn };
-public enum ObjectType { None, Cube };
+public enum ActionType { None, Turn, Color };
+public enum ObjectType { None, Cube, Sphere };
 public delegate void performActionOnObject(GameObject o);
 
 public class SpeechRecognizer : MonoBehaviour {
 
-    public GameObject target;
-    public string[] keywords = new string[]{"turn"};
-    public ConfidenceLevel confidenceLevel = ConfidenceLevel.Medium;
+    public GameObject target = null;
+    public string[] keywords = new string[]{"turn", "color", "cube", "sphere"};
+    public ConfidenceLevel confidenceLevel = ConfidenceLevel.Low;
+
+    //
+    public GameObject cube;
+    public GameObject sphere;
+    //
 
     protected PhraseRecognizer phraseRecognizer;
-    protected string actionWord = "none";
-    protected string objectWord = "none";
+    protected DictationRecognizer dictationRecognizer;
+    protected string currentWord = "none";
+    [SerializeField]
     protected ActionType action = ActionType.None;
+    [SerializeField]
     protected ObjectType obj    = ObjectType.None; 
-    performActionOnObject performAction;
+    performActionOnObject performAction = null;
 
 	// Use this for initialization
 	void Start () {
         if (keywords != null && PhraseRecognitionSystem.isSupported)
         {
             Debug.Log(PhraseRecognitionSystem.isSupported);
+            /* IMPORTANT - only works in 32bit Editor or Build (bug in Unity/Windows that will not be fixed)
+            dictationRecognizer = new DictationRecognizer();
+            dictationRecognizer.DictationResult     += dictationResultWriteText;
+            dictationRecognizer.DictationComplete   += dictationComplete;
+            dictationRecognizer.DictationError      += dictationError;
+            dictationRecognizer.DictationHypothesis += dictationHypothesis;
+            dictationRecognizer.Start(); */
             phraseRecognizer = new KeywordRecognizer(keywords, confidenceLevel);
             phraseRecognizer.OnPhraseRecognized += onPhraseRecognized;
             phraseRecognizer.Start();
@@ -45,15 +59,25 @@ public class SpeechRecognizer : MonoBehaviour {
 
     private void onPhraseRecognized(PhraseRecognizedEventArgs args)
     {
-        actionWord = args.text;
+        currentWord = args.text;
         //object = args.text....
 
         processActionName();
         processObjectName();
 
+        Debug.Log("Current word: " + currentWord +
+            "| Current object: " + obj +
+            ", current action: " + action);
+
         if (performAction != null && target != null)
         {
+            Debug.Log("Performing action: " + action + " on object: " + obj);
             performAction(target);
+            //reset the phrase
+            performAction = null;
+            action = ActionType.None;
+            target = null;
+            obj = ObjectType.None;
         }
     }
 
@@ -61,29 +85,37 @@ public class SpeechRecognizer : MonoBehaviour {
 
     private void processActionName()
     {
-        switch (actionWord)
+        switch (currentWord)
         {
             case "turn":
                 action = ActionType.Turn;
                 performAction = rotateObject;
                 break;
+            case "color":
+                action = ActionType.Color;
+                performAction = changeColor;
+                break;
             default:
-                action = ActionType.None;
-                performAction = null;
+                //action = ActionType.None;
+                //performAction = null;
                 break;
         }
     }
 
     private void processObjectName()
     {
-        switch (objectWord)
+        switch (currentWord)
         {
             case "cube":
                 obj = ObjectType.Cube;
-                //target = cube
+                target = cube;
+                break;
+            case "sphere":
+                obj = ObjectType.Sphere;
+                target = sphere;
                 break;
             default:
-                obj = ObjectType.None;
+                //obj = ObjectType.None;
                 //target = null;
                 break;
         }
@@ -98,6 +130,33 @@ public class SpeechRecognizer : MonoBehaviour {
         o.transform.Rotate(new Vector3(0,0,45.0f));
     }
 
+    private void changeColor(GameObject o)
+    {
+        o.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    /// For dictation
+
+    private void dictationResultWriteText(string text, ConfidenceLevel confidence)
+    {
+        Debug.Log("DictationResult: " + text);
+    }
+
+    private void dictationComplete(DictationCompletionCause cause)
+    {
+        Debug.Log("DictationComplete: " + cause);
+    }
+
+    private void dictationHypothesis(string text)
+    {
+        Debug.Log("DictationHypothesis: " + text);
+    }
+
+    private void dictationError(string error, int hresult)
+    {
+        Debug.Log("DictationResult: " + error + ", " + hresult);
+    }
+
     #endregion
 
     private void OnApplicationQuit()
@@ -105,6 +164,10 @@ public class SpeechRecognizer : MonoBehaviour {
         if(phraseRecognizer != null && phraseRecognizer.IsRunning)
         {
             phraseRecognizer.Stop();
+        }
+        if (dictationRecognizer != null)
+        {
+            dictationRecognizer.Stop();
         }
     }
 }
