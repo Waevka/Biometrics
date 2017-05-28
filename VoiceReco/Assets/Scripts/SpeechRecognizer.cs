87eb346f70a8a425c416fdb5edef08f464e9a92e
Waevka <waveswallow@gmail.com>
2017-05-28 21:17:24 +0200

    Added logging in.

--- VoiceReco/Assets/Scripts/SpeechRecognizer.cs
+++ VoiceReco/Assets/Scripts/SpeechRecognizer.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
@@ -16,6 +19,9 @@ public class SpeechRecognizer : MonoBehaviour {
     //
     public GameObject cube;
     public GameObject sphere;
+    private bool userLoggedIn = false;
+    private bool userAuthenticated = false;
+    private string userName = "";
     //
 
     protected PhraseRecognizer phraseRecognizer;
@@ -29,6 +35,40 @@ public class SpeechRecognizer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!userLoggedIn)
+        {
+            if (System.IO.File.Exists("login.txt"))
+            {
+                userLoggedIn = true;
+                Debug.Log("Found login file, verifying...");
+                readUserFromFile();
+                validateUser();
+            }
+        }
+	}
+
+    private void validateUser()
+    {
+        if (userName.Equals("Patryk") || userName.Equals("Bogna") || userName.Equals("Bartek"))
+        {
+            userAuthenticated = true;
+            Debug.Log("Authenticated user: " + userName);
+            initializeSpeechRecognition();
+        }
+        else
+        {
+            Debug.Log("Username " + userName + " is not valid");
+            userLoggedIn = false;
+        }
+    }
+
+    private void initializeSpeechRecognition()
+    {
         if (keywords != null && PhraseRecognitionSystem.isSupported)
         {
             Debug.Log(PhraseRecognitionSystem.isSupported);
@@ -43,19 +83,35 @@ public class SpeechRecognizer : MonoBehaviour {
             phraseRecognizer.OnPhraseRecognized += onPhraseRecognized;
             phraseRecognizer.Start();
             Debug.Log(PhraseRecognitionSystem.Status);
-        } else
+        }
+        else
         {
             if (!PhraseRecognitionSystem.isSupported)
             {
                 Debug.Log("Phrase Recognition seems to be not supported by your system. Please use Windows 10 and change your language settings to English.");
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    private void readUserFromFile()
+    {
+        var fileStream = new FileStream("login.txt", FileMode.Open, FileAccess.Read);
+        string line = "";
+        try
+        {
+            using (StreamReader sr = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                line = sr.ReadLine();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+        Debug.Log("Retrieved username: " + line);
+        userName = line;
+    }
 
     private void onPhraseRecognized(PhraseRecognizedEventArgs args)
     {
